2ddfd70b9f80ed7cb5e722c54c9c68224b9749b3
Alex Barba <leparadoxhd@gmail.com>
2019-10-02 12:30:26 +0200

    Named chunks

--- scripts/DEPLOY_DIP_PROD_TRUCKS.ps1
+++ scripts/DEPLOY_DIP_PROD_TRUCKS.ps1
@@ -1,26 +1,35 @@
 
 # AMVARA
+# 2019-10-01 ABP Modify script deploy path and better code
 # 2019-09-30 ABP Add manifest modification
 # Deploy script to DIP PROD Trucks
 
 # Backup files
 
+cd ..
+if (!(Test-Path "manifest.json" -PathType Leaf)) {
+    echo "Wrong directory, please create a 'scripts' folder behind the build files and move or copy this script inside the just created 'scripts' folder."
+    Exit
+}
+echo "DEPLOY DIP TO PROD"
+
 echo "Making backup..."
-$baseFolder = "D:\NAS\ibiss-isn-shared-prod\shared-internal\webcontent\DIPRE\"
-$backupsFolder = "D:\NAS\ibiss-isn-shared-prod\shared-internal\webcontent\DIPRE\bkp\"
+$baseFolder = "\\sedcspi1001f.emea.isn.corpintra.net\eedc_o00030\ibiss-isn-shared-prod\shared-internal\webcontent\DIPRE\"
+$backupsFolder = "$($baseFolder)bkp\"
 $time = Get-Date -UFormat "v2__%d_%m_%Y__%H_%M_%S"
-New-Item -ItemType directory -Path $backupsFolder$time
-Copy-Item -Path $baseFolder"*.*" -Destination $backupsFolder$time
-Copy-Item $baseFolder"assets" -Recurse -Destination $backupsFolder$time"\assets"
+$null = New-Item -ItemType directory -Path $backupsFolder$time
+Copy-Item -Path $baseFolder"*.*" -Destination $backupsFolder$time -ErrorAction SilentlyContinue
+Copy-Item $baseFolder"assets" -Recurse -Destination $backupsFolder$time"\assets" -ErrorAction SilentlyContinue
 
 # Copy files
 
 echo "Copying new files..."
-Get-ChildItem $baseFolder"*.*" -exclude more.html | Where { ! $_.PSIsContainer } | remove-item
-Remove-Item $baseFolder"assets" -Force -Recurse
-$codeSrc = "./src/"
-New-Item -ItemType directory -Path $baseFolder"assets"
-Copy-item -Force -Recurse -Path $codeSrc"*" -Destination $baseFolder
+Get-ChildItem $baseFolder"*.*" -exclude more.html | Where { ! $_.PSIsContainer } | remove-item -ErrorAction SilentlyContinue
+Remove-Item $baseFolder"assets" -Force -Recurse -ErrorAction SilentlyContinue
+
+$null = New-Item -ItemType directory -Path $baseFolder"assets"
+Copy-item -Force -Recurse -Path "*" -Destination $baseFolder
+Copy-item more.html -Destination $baseFolder
 
 # Edit Config JSON file and modify environment variables
 
@@ -41,5 +50,6 @@ $manifest.scope = "/internal/bi/app/DIPRE/"
 $manifest.start_url = "/internal/bi/app/DIPRE/"
 $manifest | ConvertTo-Json -Depth 50 | set-content $manifestFile
 
+cd scripts
 echo "Done!"
 Start-Sleep -s 2
--- scripts/DEPLOY_DIP_PROD_VANS.ps1
+++ scripts/DEPLOY_DIP_PROD_VANS.ps1
@@ -1,26 +1,34 @@
 
 # AMVARA
+# 2019-10-01 ABP Modify script deploy path and better code
 # 2019-09-30 ABP Add manifest modification
 # Deploy script to DIP PROD Vans
 
 # Backup files
 
+cd ..
+if (!(Test-Path "manifest.json" -PathType Leaf)) {
+    echo "Wrong directory, please create a 'scripts' folder behind the build files and move or copy this script inside the just created 'scripts' folder."
+    Exit
+}
+echo "DEPLOY DIP TO PROD"
+
 echo "Making backup..."
-$baseFolder = "D:\NAS\ibiss-isn-shared-prod\shared-internal\webcontent\DIPRE\"
-$backupsFolder = "D:\NAS\ibiss-isn-shared-prod\shared-internal\webcontent\DIPRE\vans\bkp\"
-$time = Get-Date -UFormat "v2_vans__%d_%m_%Y__%H_%M_%S"
-New-Item -ItemType directory -Path $backupsFolder$time
-Copy-Item -Path $baseFolder"*.*" -Destination $backupsFolder$time
-Copy-Item $baseFolder"assets" -Recurse -Destination $backupsFolder$time"\assets"
+$baseFolder = "\\sedcspi1001f.emea.isn.corpintra.net\eedc_o00030\ibiss-isn-shared-prod\shared-internal\webcontent\DIPRE\vans\"
+$backupsFolder = "$($baseFolder)bkp\"
+$time = Get-Date -UFormat "v2__%d_%m_%Y__%H_%M_%S"
+$null = New-Item -ItemType directory -Path $backupsFolder$time
+Copy-Item -Path $baseFolder"*.*" -Destination $backupsFolder$time -ErrorAction SilentlyContinue
+Copy-Item $baseFolder"assets" -Recurse -Destination $backupsFolder$time"\assets" -ErrorAction SilentlyContinue
 
 # Copy files
 
 echo "Copying new files..."
-Get-ChildItem $baseFolder"*.*" -exclude more.html | Where { ! $_.PSIsContainer } | remove-item
-Remove-Item $baseFolder"assets" -Force -Recurse
-$codeSrc = "./src/"
-New-Item -ItemType directory -Path $baseFolder"assets"
-Copy-item -Force -Recurse -Path $codeSrc"*" -Destination $baseFolder
+Get-ChildItem $baseFolder"*.*" -exclude more.html | Where { ! $_.PSIsContainer } | remove-item -ErrorAction SilentlyContinue
+Remove-Item $baseFolder"assets" -Force -Recurse -ErrorAction SilentlyContinue
+
+$null = New-Item -ItemType directory -Path $baseFolder"assets"
+Copy-item -Force -Recurse -Path "*" -Destination $baseFolder
 Copy-item more.html -Destination $baseFolder
 
 # Edit Config JSON file and modify environment variables
@@ -29,7 +37,7 @@ echo "Modifying cognos.json..."
 $configFile = $baseFolder+"assets\cognos.json"
 $config = Get-Content $configFile -Encoding UTF8 -Raw | ConvertFrom-Json
 $config.scenario = "prod"
-$config.target = "vans"
+$config.target = "trucks"
 $config.debug = $False
 $config | ConvertTo-Json -Depth 50 | set-content $configFile
 
@@ -38,9 +46,10 @@ $config | ConvertTo-Json -Depth 50 | set-content $configFile
 echo "Modifying manifest.json..."
 $manifestFile = $baseFolder+"manifest.json"
 $manifest = Get-Content $manifestFile -Encoding UTF8 -Raw | ConvertFrom-Json
-$manifest.scope = "/internal/bi/app/DIPRE/vans/"
-$manifest.start_url = "/internal/bi/app/DIPRE/vans/"
+$manifest.scope = "/internal/bi/app/DIPRE/"
+$manifest.start_url = "/internal/bi/app/DIPRE/"
 $manifest | ConvertTo-Json -Depth 50 | set-content $manifestFile
 
+cd scripts
 echo "Done!"
 Start-Sleep -s 2

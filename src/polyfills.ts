b423b90dc24f13fdddcd23c20dcb89f5c8975e75
Alex Barba <leparadoxhdv@gmail.com>
2020-05-18 20:01:23 +0200

    Fix polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -34,6 +34,7 @@ import 'core-js/es6/map';
 import 'core-js/es6/weak-map';
 import 'core-js/es6/set';
 import 'core-js/es6/promise';
+import 'core-js/es7/array';
 import 'zone.js';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
